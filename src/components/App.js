d525c27f3308449c98853cc9bb8f44ba9b1534a2
Rupesh Mali <rupeshpatil11121@gmail.com>
2025-10-29 01:24:33 +0530

    check it now

--- src/components/App.js
+++ src/components/App.js
@@ -115,35 +115,7 @@ export default function App() {
         <button onClick={handleStart}>Start</button>
       </div>
 
-      <div className="cells_container">
-        <h4>{tries}</h4>
-
-        {tiles.map((tile, index) => (
-          <div
-            key={tile.id}
-            className="cell"
-            style={{
-              display: "inline-block",
-              width: 60,
-              height: 60,
-              lineHeight: "60px",
-              margin: 6,
-              textAlign: "center",
-              border: "1px solid #ccc",
-              cursor: "pointer",
-              userSelect: "none",
-              backgroundColor:
-                tile.flipped || tile.matched ? "lightgreen" : "aqua",
-              fontWeight: "bold",
-              fontSize: "18px",
-            }}
-            data-index={index}
-            onClick={() => handleTileClick(tile.id, tile.value)}
-          >
-            <span>{tile.flipped || tile.matched ? tile.value : ""}</span>
-          </div>
-        ))}
-      </div>
+    
     </>
   );
 }
